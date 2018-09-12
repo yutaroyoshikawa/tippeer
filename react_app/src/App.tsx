9a0c7711df876673b7ca8b295fcf07455979f5a9
NaokiM03 <mattsun.phone@gmail.com>
2018-09-12 23:54:56 +0900

    Revert "React render test"
    This reverts commit 3c0c4fb96ea533c6db7a2e8ee821afb3ebe45e05.

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 class App extends React.Component {
   public render() {
     return (
-      <h1>Hello, Laravel-React!</h1>
+      <h1>HelloWorld</h1>
     );
   }
 }
