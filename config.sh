e5807d1daa18936c805a40344bdbb08ce8d49da8
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-10-09 20:33:35 +0900

    [FIX]dockerで実行

--- config.sh
+++ config.sh
@@ -4,5 +4,6 @@ sed -i -e '10 s/=127.0.0.1/=tipperdbinstance.cra9bcvutwgz.us-east-2.rds.amazonaw
 sed -i -e '12 s/=homestead/=tipperdb/g' $(cd $(dirname $0); pwd)/.env
 sed -i -e '13 s/=homestead/=tipperuser/g' $(cd $(dirname $0); pwd)/.env
 sed -i -e '14 s/=secret/=6fQ8wZbB/g' $(cd $(dirname $0); pwd)/.env
-php artisan key:generate
 rm $(cd $(dirname $0); pwd)/.env-e
+cd $(cd $(dirname $0); pwd)/../laradock
+docker-compose exec workspace php artisan key:generate
