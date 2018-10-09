860b0f0311823c4221a3705797459501df770e46
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-10-09 20:02:22 +0900

    recreate project

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp $(cd $(dirname $0); pwd)/.env.example $(cd $(dirname $0); pwd)/.env
+sed -i -e '10 s/=127.0.0.1/=tipperdbinstance.cra9bcvutwgz.us-east-2.rds.amazonaws.com/g' $(cd $(dirname $0); pwd)/.env
+sed -i -e '12 s/=homestead/=tipperdb/g' $(cd $(dirname $0); pwd)/.env
+sed -i -e '13 s/=homestead/=tipperuser/g' $(cd $(dirname $0); pwd)/.env
+sed -i -e '14 s/=secret/=6fQ8wZbB/g' $(cd $(dirname $0); pwd)/.env
+php artisan key:generate
+rm $(cd $(dirname $0); pwd)/.env-e
