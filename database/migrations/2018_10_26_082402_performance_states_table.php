20ce701202e634b108dfd1aeadaff70c320bb515
YusukeSato14 <usk.sugar14@gmail.com>
2018-10-31 14:08:34 +0900

    [ADD] create performance_states_table migration

--- database/migrations/2018_10_26_082402_performance_states_table.php
+++ database/migrations/2018_10_26_082402_performance_states_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class PerformanceStatesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('performance_states', function (Blueprint $table) {
+            $table -> increments('id') -> comment('主キー');
+            $table -> string('state') -> comment('パフォーマンス申請状態');
         });
     }
 
@@ -27,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('performance_states');
     }
 }
