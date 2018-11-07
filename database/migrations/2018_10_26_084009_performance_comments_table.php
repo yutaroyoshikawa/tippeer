2f327ef3f897ffe5b849ffa56b22e681386dd80f
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-07 22:26:12 +0900

    [ADD] create performance_comments_table migration

---
+++ database/migrations/2018_10_26_084009_performance_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PerformanceCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performance_comments', function (Blueprint $table) {
+            $table -> increments('id') -> comment('主キー');
+            $table -> unsignedInteger('user_id') -> comment('ユーザID');
+            $table -> string('content') -> comment('コメント内容');
+            $table -> timestamps('date') -> comment('投稿日');
+
+            $table -> foreign('user_id')
+                -> references('id')
+                -> on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performance_comments');
+    }
+}
