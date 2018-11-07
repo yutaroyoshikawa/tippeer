09b006bfa15b38a9d18ad0695ad2ce20d1d71f45
NaokiM03 <mattsun.phone@gmail.com>
2018-11-07 16:50:35 +0900

    インデックスで一覧を表示する

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Article;
 class ArticlesController extends Controller
 {
     /**
@@ -13,7 +14,8 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::all();
+        return $articles;
     }
 
     /**
