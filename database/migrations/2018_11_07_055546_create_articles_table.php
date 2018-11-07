e9541b4d5da9bb0fcad0b36d113466f4ff768e16
NaokiM03 <mattsun.phone@gmail.com>
2018-11-07 15:06:55 +0900

    title と body を追加

--- database/migrations/2018_11_07_055546_create_articles_table.php
+++ database/migrations/2018_11_07_055546_create_articles_table.php
@@ -15,6 +15,8 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
