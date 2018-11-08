4c276778cf7f1df89a6dec16235f63f5fed4b88a
NaokiM03 <mattsun.phone@gmail.com>
2018-11-08 14:14:34 +0900

    index ページに新規作成のリンクを作る

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -6,6 +6,9 @@
 
 {{-- application.blade.phpの@yield('content')に以下のレイアウトを代入 --}}
 @section('content')
+  <div>
+    <a href="/articles/create">新規作成</a>
+  </div>
   @foreach ($articles as $article)
     <h4>{{$article->title}}</h4>
     <p>{{$article->body}}</p>
