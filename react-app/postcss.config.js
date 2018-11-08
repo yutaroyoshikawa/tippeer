43cfd5496f5504f13db994fc28a99c6411b6e783
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-08 23:42:58 +0900

    [ADD]install postcss/postcss-import

--- react-app/postcss.config.js
+++ react-app/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         require('postcss-nested'),
         require('autoprefixer'),
+        require('postcss-import'),
         require('cssnano')({
             preset: 'default',
         }),
