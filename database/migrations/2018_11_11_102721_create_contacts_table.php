a3fddafbf61341fd3959f08bedd801bd3d6fb905
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 19:33:30 +0900

    [ADD]contactsテーブルの追加

---
+++ database/migrations/2018_11_11_102721_create_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id')            ->comment('主キー');
+            $table->unsignedInteger('user_id')  ->comment('ユーザID');
+            $table->string('title')             ->comment('問い合わせタイトル');
+            $table->string('content')           ->comment('問い合わせ本文');
+            $table->unsignedInteger('type_id')  ->comment('問い合わせ種ID');
+            $table->timestampsTz('date')        ->comment('問い合わせ日時');
+
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
