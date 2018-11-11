230001e6468f544350e9453a72dfa38dfb855771
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 16:56:01 +0900

    [ADD]user_tagsテーブルの作成

---
+++ database/migrations/2018_11_11_072727_create_user_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_tags', function (Blueprint $table) {
+            $table->increments('id')            -> comment('主キー');
+            $table->unsignedInteger('user_id')  -> comment('ユーザID');
+            $table->unsignedInteger('tag_id')   -> comment('タグID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_tags');
+    }
+}
