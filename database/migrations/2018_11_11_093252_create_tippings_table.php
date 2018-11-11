db5ed172826b422452fd9243200029c51ced4ceb
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 18:54:36 +0900

    [ADD]tippingsテーブルの追加

---
+++ database/migrations/2018_11_11_093252_create_tippings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTippingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tippings', function (Blueprint $table) {
+            $table->increments('id')                ->comment('主キー');
+            $table -> unsignedInteger('user_id')    ->comment('ユーザID');
+            $table -> unsignedInteger('artist_id')  ->comment('アーティストID');
+            $table -> unsignedInteger('value')      ->comment('投げ銭額');
+            $table->timestampsTz('date')            ->comment('投げ銭日時');
+
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tippings');
+    }
+}
