43f9e587fa755c59f765b100d80ce9398ec8e807
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 17:13:40 +0900

    [ADD]search_historiesテーブルの追加

---
+++ database/migrations/2018_11_11_080203_create_search_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSearchHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('search_histories', function (Blueprint $table) {
+            $table->increments('id')            ->comment('主キー');
+            $table->unsignedInteger('user_id')  ->comment('ユーザID');
+            $table->string('key_word')          ->comment('検索キーワード');
+            $table->timestampsTz('date')        ->comment('検索日時');
+
+            $table -> index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('search_histories');
+    }
+}
