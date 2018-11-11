42666a89f8890e70dbe4663def3611ae2f49ebea
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 16:56:18 +0900

    [ADD]works_tagsテーブルの作成

---
+++ database/migrations/2018_11_11_073347_create_works_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works_tags', function (Blueprint $table) {
+            $table->increments('id')            -> comment('主キー');
+            $table->unsignedInteger('user_id')  -> comment('アーティストID');
+            $table->unsignedInteger('tag_id')   -> comment('タグID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works_tags');
+    }
+}
