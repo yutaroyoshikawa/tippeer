6e26899bb0a29241dfa384e333171d0bc8dd65c8
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 16:55:30 +0900

    [ADD]artist_tagsテーブルの作成

---
+++ database/migrations/2018_11_11_074726_create_artist_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtistTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artist_tags', function (Blueprint $table) {
+            $table->increments('id')            -> comment('主キー');
+            $table->unsignedInteger('user_id')  -> comment('アーティストID');
+            $table->unsignedInteger('tag_id')   -> comment('タグID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artist_tags');
+    }
+}
