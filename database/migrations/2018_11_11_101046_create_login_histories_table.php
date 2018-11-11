6cb95d7c876e3ea4011b1404286b5433dd9fb2f6
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 19:21:41 +0900

    [ADD]login_historiesテーブルの追加

---
+++ database/migrations/2018_11_11_101046_create_login_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login_histories', function (Blueprint $table) {
+            $table->increments('id')            ->comment('主キー');
+            $table->unsignedInteger('user_id')  ->comment('ユーザID');
+            $table->string('ip_address')        ->comment('ログインIPアドレス');
+            $table->timestampsTz('date')        ->comment('ログイン日時');
+
+            $table->index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login_histories');
+    }
+}
