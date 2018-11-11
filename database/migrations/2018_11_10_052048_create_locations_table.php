b85d4b61f6bd6b68e6a74f748196af9361a6e5c1
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 16:02:41 +0900

    [ADD]locationsテーブルの作成

---
+++ database/migrations/2018_11_10_052048_create_locations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id')                    -> comment('主キー');
+            $table->string('name')                      -> comment('地名');
+            $table->unsignedInteger('prefecture_id')    -> comment('都道府県ID');
+            $table->string('address')                   -> comment('住所');
+            $table->float('latitude')                   -> comment('緯度');
+            $table->float('longitude')                  -> comment('経度');
+
+            $table -> index('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
