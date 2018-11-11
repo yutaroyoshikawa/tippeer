16f0de49566e2e9407e8caa25dea5fea311be617
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-11 19:59:28 +0900

    [FIX]スペルミスの修正

--- database/migrations/2100_12_31_235959_create_performance_equipments_relations.php
+++ database/migrations/2100_12_31_235959_create_performance_equipments_relations.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePerformanceEquipmantsRelations extends Migration
+class CreatePerformanceEquipmentsRelations extends Migration
 {
     /**
      * Run the migrations.
