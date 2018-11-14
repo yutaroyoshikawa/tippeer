91a427bb0f43df7497e35854aab9dd5eb47bbcb0
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-14 15:44:58 +0900

    [FIX]artist_tagsテーブルのカラム名変更

--- database/migrations/2018_11_11_074726_create_artist_tags_table.php
+++ database/migrations/2018_11_11_074726_create_artist_tags_table.php
@@ -14,9 +14,9 @@ class CreateArtistTagsTable extends Migration
     public function up()
     {
         Schema::create('artist_tags', function (Blueprint $table) {
-            $table->increments('id')            -> comment('主キー');
-            $table->unsignedInteger('user_id')  -> comment('アーティストID');
-            $table->unsignedInteger('tag_id')   -> comment('タグID');
+            $table->increments('id')                -> comment('主キー');
+            $table->unsignedInteger('artist_id')    -> comment('アーティストID');
+            $table->unsignedInteger('tag_id')       -> comment('タグID');
 
             $table -> index('id');
         });
