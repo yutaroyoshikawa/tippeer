e865fa855485d5ec6c21e293fe8423f908dd4151
reijihukutomi <gd00.puku3@gmail.com>
2018-11-15 14:53:30 +0900

    [UPDATE]apiルーティング機能追加

--- routes/api.php
+++ routes/api.php
@@ -17,25 +17,29 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-Route::get( '/foo', function () {
+// Route::get( '/foo', function () {
 
-    $pitchers = collect([
-        [
-            'id'  => '1',
-            'name' => 'first',
-            'email'  => 'hoge@sample.com',
-        ],
-        [
-            'id'  => '2',
-            'name' => 'second',
-            'email'  => 'fuga@sample.com',
-        ],
-        [
-            'id'  => '3',
-            'name' => 'third',
-            'email'  => 'piyo@sample.com',
-        ],
-    ]);
+//     $pitchers = collect([
+//         [
+//             'id'  => '1',
+//             'name' => 'first',
+//             'email'  => 'hoge@sample.com',
+//         ],
+//         [
+//             'id'  => '2',
+//             'name' => 'second',
+//             'email'  => 'fuga@sample.com',
+//         ],
+//         [
+//             'id'  => '3',
+//             'name' => 'third',
+//             'email'  => 'piyo@sample.com',
+//         ],
+//     ]);
 
-    return response()->json( $pitchers );
-} );
+//     return response()->json( $pitchers );
+// } );
+
+Route::group(['middleware' => ['api']], function(){
+    Route::get('search', 'SearchController@show');
+});
