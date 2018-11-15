8ebb4b0d50a15c96df59f8b25495641e0365a6cb
reijihukutomi <gd00.puku3@gmail.com>
2018-11-15 15:23:13 +0900

    [UPDATE[Artists_testテーブル作成

---
+++ app/Artists_test.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artists_test extends Model
+{
+    protected $table = 'artists_test';
+
+    protected $guarded = array('id');
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'artist_name','twitter_id'
+    ];
+
+    /**
+     * パフォーマンスの取得
+     */
+
+    public function Performances_test() {
+        return $this -> hasMany('App\Performances_test');
+    }
+}
