948bfbbb81a5fce98d57d880b9a1d2a7140eb9aa
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-18 13:28:54 +0900

    [FIX]Tipperロゴをsvgに変更

--- react-app/src/components/tipperLogo.tsx
+++ react-app/src/components/tipperLogo.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
+import ReactSVG from 'react-svg'
 import { Dispatch } from 'redux'
 import { ITipperLogoState } from 'src/reducers/tipperLogo'
-import Tippericon from '../Tippericon.png'
+import TipperIcon from '../TipperIcon.svg'
 
 interface IProps extends ITipperLogoState {
     dispatch: Dispatch<any>;
@@ -19,7 +20,7 @@ export default class extends React.Component<IProps, IState> {
     public render() {
         return(
             <li>
-                <img src={Tippericon} alt="TipperLogo" style={{'width': '50px'}}/>
+                <ReactSVG src={TipperIcon} svgStyle={{ width: 50 }} />
             </li>
         )
     }
