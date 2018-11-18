7e555b1cc7fe904e03a65c937824ec9c7c9c0c29
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-11-18 12:16:26 +0900

    [ADD]combineReducerにGlobalMenuのreducerを追加

--- react-app/src/reducers/index.ts
+++ react-app/src/reducers/index.ts
@@ -1,4 +1,12 @@
 import { combineReducers } from 'redux';
+import back from './back'
+import searchBox from './searchBox'
+import tipperLogo from './tipperLogo'
+import userMenu from './userMenu'
 
 export default combineReducers({
+    back,
+    searchBox,
+    tipperLogo,
+    userMenu
 });
