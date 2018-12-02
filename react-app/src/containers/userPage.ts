d40dd9a484a9f73978f49ab18236c43e7a259d6e
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-12-02 16:59:10 +0900

    [ADD]ユーザページの仮作成

---
+++ react-app/src/containers/userPage.ts
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
+import userPage from '../layouts/userPage'
+import {buildStore} from '../store'
+
+const store = buildStore()
+type AllState = ReturnType<typeof store.getState>
+
+const mapStateToProps = (state: AllState) => {
+    return {
+        globalMenu: state.globalMenu.globalMenu
+    }
+}
+const mapDispatchToProps = (dispatch: Dispatch) => ({ dispatch })
+export const UserPage = connect<{}, {}>(mapStateToProps, mapDispatchToProps)(userPage) as React.ComponentClass
