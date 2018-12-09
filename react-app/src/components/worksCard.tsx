2498731072a6753a21c8568eaa38238170880f11
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-12-09 13:47:29 +0900

    [ADD]performanceCardテンプレート作成

--- react-app/src/components/worksCard.tsx
+++ react-app/src/components/worksCard.tsx
@@ -2,6 +2,9 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { ArtistCard } from './'
 
+import nature from '../natureExample.jpeg'
+
+
 export interface IProps {
     worksId: number
 }
@@ -19,6 +22,14 @@ export interface IState {
 export class WorksCard extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
+
+        this.state = ({
+            artistId: '',
+            loadState: 'none',
+            price: 0,
+            worksName: '', 
+            worksThumbnail: '',
+        })
     }
 
     public componentDidMount() {
@@ -33,16 +44,16 @@ export class WorksCard extends React.Component<IProps, IState> {
 
     public render() {
         return(
-            <Link to={"/works/" + this.props.worksId}  >
-                <article>
-                    <p><img src={this.state.worksThumbnail} alt="worksThumbnail"/></p>
-                    <h3>{this.state.worksName}</h3>
-                    <Link to={'/artists/' + this.state.artistId}>
-                        <ArtistCard artistId={this.state.artistId} />
-                    </Link>
-                    <p>{this.state.price}</p>
-                </article>
-            </Link>
+            <article style={{backgroundColor: 'white', padding: '30px 20px 50px 20px', filter: 'drop-shadow(0 0 2px #555)', margin: '5px 20px', width: '200px', marginBottom: '75px'}}>
+                <Link to={"/works/" + this.props.worksId}  >
+                <p><img src={nature} style={{width: '100%'}} alt="worksThumbnail"/></p>
+                <h3 style={{fontWeight: 'normal', fontSize: '20px', color: '#666', marginBottom: '20px', textAlign: 'center'}}>{this.state.worksName}</h3>
+                </Link>
+                <Link to={'/artists/' + this.state.artistId} style={{}}>
+                    <ArtistCard artistId={this.state.artistId} size={40} style={'card'} />
+                </Link>
+                <p style={{display: 'flex', width: '80px', height: '80px', borderRadius: '100px', backgroundColor: 'rgb(159,224,192)', justifyContent: 'center', alignItems: 'center', color: '#333', position: 'absolute', transform: 'translateX(50%)'}}>￥{this.state.price}</p>
+            </article>
         )
     }
 }
