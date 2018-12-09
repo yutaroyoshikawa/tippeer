45fccf7cdb11e49007dbf67de819b5a81b6a49f8
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-12-09 13:51:41 +0900

    [FIX]searchページへのルーティングを作成

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,8 +5,8 @@ import { Route, Router, Switch } from 'react-router-dom';
 import 'ress'
 import { Faq, PrivacyPolicy, UserPage } from './components'
 import { GlobalMenu } from './containers/globalMenu'
+import { Search } from './layouts'
 import { buildStore } from './store';
-
 
 const store = buildStore();
 const history = createHistory();
@@ -18,13 +18,15 @@ class App extends React.Component {
                 <Router history={history}>
                     <div>
                         <GlobalMenu />
-                        <Switch>
-                            <div style={{padding: '50px 0'}}>
-                            <Route exact={true} path='/faq' component={Faq} />
-                            <Route exact={true} path='/privacypolicy' component={PrivacyPolicy} />
-                            <Route exact={true} path='/' component={UserPage} />
-                            </div>
-                        </Switch>
+                        <div style={{padding: '50px 0'}} >
+                            <Switch>
+                                <Route exact={true} path='/faq' component={Faq} />
+                                <Route exact={true} path='/privacypolicy' component={PrivacyPolicy} />
+                                <Route exact={true} path='/' component={UserPage} />
+                                <Route exact={true} path='/search' component={Search} />
+                                <Route exact={true} path='/search/:searchWord' component={Search} />
+                            </Switch>
+                        </div>
                     </div>
                 </Router>
             </Provider>
