d4e4cb30fe5aced8e8e4b41d63d1bfb3aeee7420
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-12-09 13:45:55 +0900

    [ADD]performanceCardテンプレート作成

--- react-app/src/components/performanceCard.tsx
+++ react-app/src/components/performanceCard.tsx
@@ -22,6 +22,17 @@ export interface IState {
 export class PerformanceCard extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
+        this.state = ({
+            address: '',
+            discription: '',
+            finish: '',
+            loadState: 'none',
+            performanceTitle: '',
+            placeId: 0,
+            placeName: '',
+            postalCode: '',
+            start: '',
+        })
     }
 
     public componentDidMount() {
@@ -40,19 +51,23 @@ export class PerformanceCard extends React.Component<IProps, IState> {
 
     public render() {
         return(
-            <Link to={"/performance/" + this.props.performanceId}  >
-                <article>
-                    <h3>{this.state.performanceTitle}</h3>
-                    <p>start<time>{this.state.start}</time></p>
-                    <p>finish<time>{this.state.finish}</time></p>
-                    <p>{this.state.discription}</p>
-                    <Link to={"/places/" + this.state.placeId}>
-                        <p>{this.state.placeName}</p>
-                        <p>〒{this.state.postalCode}</p>
-                        <p>{this.state.address}</p>
-                    </Link>
-                </article>
-            </Link>
+            <article style={{backgroundColor: 'white', padding: '30px 20px', filter: 'drop-shadow(0 0 2px #555)', margin: '5px 10px', width: '200px'}}>
+                <Link to={"/performance/" + this.props.performanceId}  >
+                    <div style={{color: '#999'}}>
+                        <h3 style={{fontWeight: 'normal', fontSize: '30px', color: '#666', marginBottom: '20px'}}>{this.state.performanceTitle}</h3>
+                        <p style={{fontSize: '12px'}}>start<time>{this.state.start}</time></p>
+                        <p style={{fontSize: '12px'}}>finish<time>{this.state.finish}</time></p>
+                        <p style={{margin: '10px 0'}}>{this.state.discription}</p>
+                    </div>  
+                </Link>
+                <Link to={"/places/" + this.state.placeId}>
+                    <div>
+                        <p style={{fontSize: '25px', color: '#666'}}>{this.state.placeName}</p>
+                        <p style={{color: '#999', fontSize: '12px'}}>〒{this.state.postalCode}</p>
+                        <p style={{color: '#999', fontSize: '12px'}}>{this.state.address}</p>
+                    </div>
+                </Link>                
+            </article>
         )
     }
 }
