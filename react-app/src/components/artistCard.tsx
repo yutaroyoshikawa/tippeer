55361525bcf1f2303df12a4f0b6974a78b4239db
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-12-09 13:46:58 +0900

    [ADD]artistCardテンプレート作成

--- react-app/src/components/artistCard.tsx
+++ react-app/src/components/artistCard.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
+import icon from '../natureicon.jpg'
+
 export interface IProps {
     artistId: string
+    size: number
+    style: 'standalone' | 'card'
 }
 
 export interface IState {
@@ -13,6 +17,11 @@ export interface IState {
 export class ArtistCard extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
+
+        this.state = ({
+            artistName: 'hoge',
+            icon: 'hoge',
+        })
     }
 
     public componentDidMount = () => {
@@ -24,12 +33,12 @@ export class ArtistCard extends React.Component<IProps, IState> {
 
     public render() {
         return(
-            <Link to={"/artists/" + this.props.artistId}  >
-                <article>
-                    <figure><img src={this.state.icon} alt="ArtistIcon"/></figure>
-                    <p>{this.state.artistName}</p>
-                </article>
-            </Link>
+            <article style={{width: '100%', textAlign: 'center', margin: '0px'}}>
+                <Link to={"/artists/" + this.props.artistId} className={this.props.style === 'card' ? 'card': 'standalone'} >
+                <figure style={{display: 'block', filter: 'drop-shadow(0 0 1px #555)'}} className={this.props.style === 'card' ? 'card-margin': 'standalone-margin'}><img src={icon} style={{width: this.props.size + 'px', height: this.props.size + 'px', borderRadius: this.props.size + 'px'}} alt="ArtistIcon"/></figure>
+                <p style={{color: '#555', display: 'block'}}>{this.state.artistName}</p>
+                </Link>
+            </article>
         )
     }
 }
