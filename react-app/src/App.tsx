7633e1a6757bd43f5b97a62c3a4d915ced4aac06
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-12-13 11:59:38 +0900

    [FIX]worksDetailsをルーティングに追加

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,7 +5,7 @@ import { Route, Router, Switch } from 'react-router-dom';
 import 'ress'
 import { Faq, PrivacyPolicy, UserPage } from './components'
 import { GlobalMenu } from './containers/globalMenu'
-import { PlaceDetails, Search } from './layouts'
+import { PlaceDetails, Search, WorksDetails } from './layouts'
 import { buildStore } from './store';
 
 const store = buildStore();
@@ -26,6 +26,7 @@ class App extends React.Component {
                                 <Route exact={true} path='/search' component={Search} />
                                 <Route exact={true} path='/search/:searchWord' component={Search} />
                                 <Route exact={true} path='/places/:placeId' component={PlaceDetails} />
+                                <Route exact={true} path='/works/:worksId' component={WorksDetails} />
                             </Switch>
                         </div>
                     </div>
