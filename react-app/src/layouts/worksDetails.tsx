3a50883a117508a729a791aefc6248ee82e7429c
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-12-27 22:38:13 +0900

    [FIX]worksDetailsのレスポンシブ対応

--- react-app/src/layouts/worksDetails.tsx
+++ react-app/src/layouts/worksDetails.tsx
@@ -5,6 +5,8 @@ import { ArticleTitle } from '../components'
 import { ArtistCard, CommentBox, CommentList, PriceCard } from '../components'
 import { IWorksDetailsState } from '../reducers/worksDetails'
 
+import * as Styled from '../styles/worksDetails'
+
 export interface IProps extends IWorksDetailsState {
     dispatch: Dispatch<any>
 }
@@ -29,51 +31,51 @@ export default class extends React.Component<IProps, {}> {
         this.props.worksDetails.contents.map((data,key) => (
             <div key={key}>
                 <li>
-                    <ul style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', listStyle: 'none'}}>
-                        <li style={{width: '60%'}}>{data.title}</li>
-                        <li style={{width: '10%'}}><ArtistCard artistId={data.artistId} size={40} style={'card'} nameHidden={false} /></li>
-                        <li style={{width: '30%', textAlign: 'right'}}><PriceCard type={'ellipse'} price={data.price} size={20} /></li>
-                    </ul>
+                    <Styled.Contents>
+                        <Styled.ContentTitle>{data.title}</Styled.ContentTitle>
+                        <Styled.ContentArtist><ArtistCard artistId={data.artistId} size={40} style={'card'} nameHidden={false} /></Styled.ContentArtist>
+                        <Styled.ContentPrice><PriceCard type={'ellipse'} price={data.price} size={20} /></Styled.ContentPrice>
+                    </Styled.Contents>
                 </li>
-                {key !== this.props.worksDetails.contents.length - 1 ? <hr style={{border: '0 none', margin: '8px 0', width: '95%', background: 'linear-gradient(90deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0))', height: '1px'}} /> : null}
+                {key !== this.props.worksDetails.contents.length - 1 ? <Styled.ContentsBorder /> : null}
             </div>
         ))
     )
 
     public render() {
         return(
-            <section className={'worksDetailsBody'} style={{width: '1020px', margin: '0 auto'}}>
-                <div style={{width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'center'}}>
-                    <div style={{display: 'flex'}}>
-                        <div style={{width: '300px', height: '300px', display: 'flex', alignItems: 'center', filter: 'drop-shadow(0 0 2px #555)'}}>
-                            <div style={{width: '300px'}}>
+            <Styled.Entire className={'worksDetailsBody'}>
+                <Styled.WorksInfo>
+                    <Styled.TopWorksInfo>
+                        <Styled.WorksThumbnail>
+                            <Styled.InnerWorksThumbnail>
                                 <img style={{width: '100%'}} src={this.props.worksDetails.worksThumbnail} alt="worksThumbnail"/>
-                            </div>
-                        </div>
-                        <div style={{marginLeft: '30px', display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'space-between'}}>
-                            <h2 style={{fontWeight: 'normal', fontSize: '60px'}}>{this.props.worksDetails.worksTitle}</h2>
+                            </Styled.InnerWorksThumbnail>
+                        </Styled.WorksThumbnail>
+                        <Styled.WorksDetails>
+                            <Styled.WorksTitle>{this.props.worksDetails.worksTitle}</Styled.WorksTitle>
                             <ArtistCard artistId={this.props.worksDetails.artistId} size={50} style={'card'} nameHidden={false} />
                             <PriceCard type={'ellipse'} price={this.props.worksDetails.price} size={30} />
                             <Score size={40} />
-                        </div>
-                    </div>
-                    <p style={{margin: '50px 0'}}>{this.props.worksDetails.description}</p>
-                    <div style={{margin: '50px 0'}}>
+                        </Styled.WorksDetails>
+                    </Styled.TopWorksInfo>
+                    <Styled.Description>{this.props.worksDetails.description}</Styled.Description>
+                    <Styled.ContentSection>
                         <ArticleTitle title={'Contents'} />
-                        <ul style={{listStyle: 'none', fontSize: '20px'}}>
+                        <Styled.ContentList>
                             {this.renderContents()}
-                        </ul>
-                    </div>
-                </div>
+                        </Styled.ContentList>
+                    </Styled.ContentSection>
+                </Styled.WorksInfo>
                 
-                <div style={{marginBottom: '50px'}}>
+                <Styled.CommentSection>
                     <ArticleTitle title={'Comments'} />
                     <CommentBox type={'works'} />
-                    <div style={{listStyle: 'none', width: '65%', margin: '0 auto'}}>
+                    <Styled.Comments>
                         <CommentList initialWorksComments={this.props.worksDetails.comments} initialPerformanceComments={null} type={'works'} dark={true} />
-                    </div>
-                </div>
-            </section>
+                    </Styled.Comments>
+                </Styled.CommentSection>
+            </Styled.Entire>
         )
     }
 }
---
+++ react-app/src/styles/worksDetails.ts
@@ -0,0 +1,182 @@
+import styled from './styled-components'
+
+export const Entire = styled.div`
+    @media screen and (min-width: 1024px){
+        width: 1020px;
+        margin: 0 auto;
+    }
+    @media screen and (max-width: 480px){
+        width: 100%;
+    }
+`
+
+export const WorksInfo = styled.section`
+    @media screen and (min-width: 1024px){
+        width: 100%;
+        height: 10vh;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    @media screen and (max-width: 480px){
+        width: 98%;
+        margin: 0 auto;
+    }
+`
+
+export const TopWorksInfo = styled.div`
+    @media screen and (min-width: 1024px){
+        display: flex;
+    }
+    @media screen and (max-width: 480px){
+        width: 98%;
+        height: 40vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`
+
+export const WorksThumbnail = styled.div`
+    display: flex;
+    align-items: center;
+    filter: drop-shadow(0 0 2px #555);
+
+    @media screen and (min-width: 1024px){
+        width: 300px;
+        height: 300px;
+    }
+    @media screen and (max-width: 480px){
+        width: 35vw;
+        height: 35vw;
+    }
+`
+
+export const InnerWorksThumbnail = styled.div`
+    @media screen and (min-width: 1024px){
+        width: 300px;
+    }
+    @media screen and (max-width: 480px){
+        width: 35vw;
+    }
+`
+
+export const WorksDetails = styled.div`
+    @media screen and (min-width: 1024px){
+        margin-left: 30px;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+    @media screen and (max-width: 480px){
+        margin-left: 20px;
+    }
+`
+
+export const WorksTitle = styled.h2`
+    font-weight: normal;
+
+    @media screen and (min-width: 1024px){
+        font-size: 60px;
+    }
+    @media screen and (max-width: 480px){
+        font-size: 30px;
+    }
+`
+
+export const Description = styled.p`
+    @media screen and (min-width: 1024px){
+        margin: 50px
+    }
+    @media screen and (max-width: 480px){
+        margin: -8px 20px 20px 20px;
+    }
+`
+
+export const ContentSection = styled.section`
+    @media screen and (min-width: 1024px){
+        margin: 50p 0;
+    }
+    @media screen and (max-width: 480px){
+        margin-bottom: 20px;
+    }
+`
+
+export const ContentList = styled.ul`
+    list-style: none;
+    font-size: 20px;
+`
+
+export const Contents = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    list-style: none;
+`
+
+export const ContentTitle = styled.li`
+    @media screen and (min-width: 1024px){
+        width: 60%;
+    }
+    @media screen and (max-width: 480px){
+        width: 35%;
+        font-size: 16px;
+    }
+`
+
+export const ContentArtist = styled.li`
+    @media screen and (min-width: 1024px){
+        width: 10%;
+    }
+    @media screen and (max-width: 480px){
+        width: 20%;
+    }
+`
+
+export const ContentPrice = styled.li`
+    text-align: center;
+
+    @media screen and (min-width: 1024px){
+        width: 30%;
+    }
+    @media screen and (max-width: 480px){
+        width: 30%;
+    }
+`
+
+export const ContentsBorder = styled.hr`
+    border: 0 none;
+    width: 95%;
+    background: linear-gradient(90deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+    height: 1px;
+
+    @media screen and (min-width: 1024px){
+        margin: 8px 0;
+    }
+    @media screen and (max-width: 480px){
+        margin: 4px 0;
+    }
+`
+
+export const CommentSection = styled.section`
+    @media screen and (min-width: 1024px){
+        margin-bottom: 50px;
+    }
+    @media screen and (max-width: 480px){
+        margin-bottom: 20px;
+    }
+`
+
+export const Comments = styled.div`
+    list-style: none;
+    margin: 0 auto;
+
+    @media screen and (min-width: 1024px){
+        width: 65%;
+    }
+    @media screen and (max-width: 480px){
+        width: 95%;
+    }
+`
