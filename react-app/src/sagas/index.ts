74ed11fdd19da5d071d7ebdb63ddc15edaf6d8fd
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-01-09 18:45:36 +0900

    [ADD]会員登録関連のsagaをindexに追加

--- react-app/src/sagas/index.ts
+++ react-app/src/sagas/index.ts
@@ -1,11 +1,15 @@
 import { SagaIterator } from 'redux-saga'
 import { all } from 'redux-saga/effects';
 import Auth from './auth'
+import FireStore from './fireStore'
+import RegistUser from './registUser'
 import Search from './search'
 
 export default function* (): SagaIterator{
   yield all([
     ...Auth,
+    ...FireStore,
+    ...RegistUser,
     ...Search,
   ])
 }
