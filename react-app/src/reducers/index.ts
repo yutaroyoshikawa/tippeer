4e36e81c69e167c7dda8044e9b9ad57cddb3b792
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-01-09 18:46:45 +0900

    [ADD]通知関連のreducerをindexに追加

--- react-app/src/reducers/index.ts
+++ react-app/src/reducers/index.ts
@@ -1,10 +1,13 @@
+import {reducer as notifications} from 'react-notification-system-redux';
 import { combineReducers } from 'redux';
 import artistDetails from './artistDetails'
 import auth from './auth'
 import back from './back'
+import dialog from './dialog'
 import globalMenu from './globalMenu'
 import performanceDetails from './performaceDetails'
 import placeDetails from './placeDetails'
+import registUser from './registUser'
 import searchBox from './searchBox'
 import tipping from './tipping'
 // import tipperLogo from './tipperLogo'
@@ -13,13 +16,17 @@ import userPage from './userPage';
 import worksDetails from './worksDetails'
 import worksTop from './worksTop'
 
+
 export default combineReducers({
     artistDetails,
     auth,
     back,
+    dialog,
     globalMenu,
+    notifications,
     performanceDetails,
     placeDetails,
+    registUser,
     searchBox,
     tipping,
     // tipperLogo,
