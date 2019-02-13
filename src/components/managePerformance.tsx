5029d2ad3a695a61003415dd1fac311e16b35f63
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-02-13 16:57:55 +0900

    [FIX]ダイアログの実装

--- src/components/managePerformance.tsx
+++ src/components/managePerformance.tsx
@@ -1,6 +1,7 @@
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Card, CardContent, Divider, Fab, Typography } from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core'
 // import ListIcon from '@material-ui/icons/List'
 import * as React from 'react'
 import { Dispatch } from 'redux'
@@ -13,10 +14,76 @@ export interface IProps extends IManageState {
     dispatch: Dispatch<any>
 }
 
-export default class extends React.Component<IProps, {}> {
+interface IState {
+    isOpenDiscribe: boolean
+}
+
+export default class extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
+
+        this.state = {
+            isOpenDiscribe: false
+        }
     }
+
+    public renderForm = () => (
+        <Dialog
+            open={this.state.isOpenDiscribe}
+            onClose={this.isOpenDialog}
+        >
+            <DialogTitle>パフォーマンス情報</DialogTitle>
+            <Divider />
+            <DialogContent>
+                <div>
+                    <TextField
+                        label="パフォーマンス名"
+                        variant="standard"
+                        margin="normal"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        label="開始時間"
+                        variant="standard"
+                        margin="normal"
+                        type="datetime-local"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        label="終了時間"
+                        variant="standard"
+                        margin="normal"
+                        type="datetime-local"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </div>
+                <div>
+                    <label>
+                        <img src=""/>
+                        <input style={{display: 'none'}} type="file"/>
+                    </label>
+                </div>
+            </DialogContent>
+            <Divider />
+            <DialogActions>
+                <Button onClick={this.isOpenDialog}>キャンセル</Button>
+                <Button>登録</Button>
+            </DialogActions>
+        </Dialog>
+    )
+
+    public isOpenDialog = () => (
+        this.setState({
+            isOpenDiscribe: this.state.isOpenDiscribe ? false :true,
+        })
+    )
 
     public render() {
         return (
@@ -26,10 +93,16 @@ export default class extends React.Component<IProps, {}> {
                         <ArticleTitle title="パフォーマンス" color="light" />
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center', margin: '50px 0' }}>
-                        <Fab color="secondary" variant="extended" aria-label="Add" >
+                        <Fab
+                            color="secondary"
+                            variant="extended"
+                            aria-label="Add"
+                            onClick={this.isOpenDialog}
+                        >
                             <FontAwesomeIcon icon={faPlusCircle} />
                             <span style={{ paddingLeft: '5px' }}>パフォーマンスを登録する</span>
                         </Fab>
+                        {this.renderForm()}
                     </div>
                     <Divider />
                     <Typography component="h3">
--- src/components/manageWorks.tsx
+++ src/components/manageWorks.tsx
@@ -1,35 +1,120 @@
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Card, CardContent, Divider, Fab, Typography } from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core'
 // import ListIcon from '@material-ui/icons/List'
 import * as React from 'react'
+import Img from 'react-image'
 import { Dispatch } from 'redux'
+import { openCropper } from '../actions/cropper'
 // import * as actions from '../actions/manage'
+import { Cropper } from '../components'
+import { ICropperState } from '../reducers/cropper'
 import { IManageState } from '../reducers/manage'
 import { ArticleTitle } from './'
 // import * as Styled from '../styles/tipping'
 
-export interface IProps extends IManageState {
+export interface IProps extends IManageState, ICropperState {
     dispatch: Dispatch<any>
 }
 
-export default class extends React.Component<IProps, {}> {
+interface IState {
+    isOpenDiscribe: boolean
+}
+
+export default class extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
+
+        this.state = {
+            isOpenDiscribe: false
+        }
     }
+
+    public setIconValue(e: React.ChangeEvent<HTMLInputElement>) {
+        const files = e.target.files
+        if (files && files[0]) {
+            const reader = new FileReader()
+            reader.onloadend = () => {
+                if (reader.result && files) {
+                    this.props.dispatch(openCropper(
+                        {
+                            data: reader.result.toString(),
+                            type: 'works',
+                        }
+                    ))
+                }
+            }
+            reader.readAsDataURL(files[0])
+        }
+        e.target.value = ''
+    }
+
+    public renderForm = () => (
+        <Dialog
+            open={this.state.isOpenDiscribe}
+            onClose={this.isOpenDialog}
+        >
+            <DialogTitle>作品情報</DialogTitle>
+            <Divider />
+            <DialogContent>
+                <div>
+                    <TextField
+                        label="作品名"
+                        variant="standard"
+                        margin="normal"
+                    />
+                </div>
+                <div>
+                    <label>
+                        <span>
+                            サムネイルを設定
+                        </span>
+                        <Img
+                            src={this.props.cropper.data}
+                            // unloader={}
+                        />
+                        <input
+                            style={{display: 'none'}}
+                            type="file"
+                            onChange={this.setIconValue.bind(this,)}
+                        />
+                    </label>
+                </div>
+            </DialogContent>
+            <Divider />
+            <DialogActions>
+                <Button onClick={this.isOpenDialog}>キャンセル</Button>
+                <Button>登録</Button>
+            </DialogActions>
+        </Dialog>
+    )
+
+    public isOpenDialog = () => (
+        this.setState({
+            isOpenDiscribe: this.state.isOpenDiscribe ? false :true,
+        })
+    )
 
     public render() {
         return (
             <div>
+                <Cropper />
                 <div style={{ width: '90%', margin: '0 auto' }}>
                     <div style={{ margin: '50px 0' }}>
                         <ArticleTitle title="作品" color="light" />
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center', margin: '50px 0' }}>
-                        <Fab color="secondary" variant="extended" aria-label="Add" >
+                        <Fab
+                            color="secondary"
+                            variant="extended"
+                            aria-label="Add"
+                            onClick={this.isOpenDialog}
+                        >
                             <FontAwesomeIcon icon={faPlusCircle} />
                             <span style={{ paddingLeft: '5px' }}>作品を登録する</span>
                         </Fab>
+                        {this.renderForm()}
                     </div>
                     <Divider />
                     <Typography component="h3">
