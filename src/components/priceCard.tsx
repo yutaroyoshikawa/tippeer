9d93f8aa98959e90c42b546ea6386bd650b0b17e
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-02-13 16:56:48 +0900

    [FIX]paymentRequestApiの導入

--- src/components/priceCard.tsx
+++ src/components/priceCard.tsx
@@ -1,5 +1,61 @@
+// import PaymentRequest from 'appr-wrapper'
 import * as React from 'react'
 import * as Styled from '../styles/components/priceCard'
+
+const merchantId = 'merchant.com.agektmr.payment';
+
+const methods = [{
+    data: {
+        supportedNetworks: [
+            'visa', 'mastercard', 'amex', 'discover',
+            'diners', 'jcb', 'unionpay'
+        ]
+    },
+    supportedMethods: 'basic-card',
+}, {
+    data: {
+        countryCode: 'JP',
+        currencyCode: 'JPY',
+        merchantCapabilities: ['supports3DS'],
+        merchantIdentifier: merchantId,
+        supportedNetworks: [
+            'amex', 'discover', 'masterCard', 'visa', 'jcb', 'privateLabel'
+        ],
+        version: 3,
+    },
+    supportedMethods: 'https://apple.com/apple-pay',
+}, {
+    supportedMethods: 'https://bobpay.xyz/pay'
+}];
+
+const details = {
+    displayItems: [{
+        amount: { currency: 'JPY', value: '100' },
+        label: 'Original donation amount',
+    }],
+    shippingOptions: [{
+        amount: { currency: 'JPY', value: '100' },
+        id: 'standard',
+        label: 'Standard shipping',
+    }, {
+        amount: { currency: 'JPY', value: '100' },
+        id: 'express',
+        label: 'Express shipping',
+    }],
+    total: {
+        amount: { currency: 'JPY', value: '100' },
+        label: 'Total due',
+
+    }
+};
+
+const applePay =
+    new PaymentRequest(methods, details).canMakePayment ?
+        new PaymentRequest(methods, details)
+        :
+        null
+
+
 
 export interface IProps {
     price: number
@@ -12,16 +68,25 @@ export class PriceCard extends React.Component<IProps, {}> {
         super(props)
     }
 
+    public hundleClick = () => (
+        applePay ?
+            applePay.show().then(result => (
+                result.complete('success')
+            ))
+            :
+            null
+    )
+
     public renderPriceCard = () => (
-        this.props.type === 'circle' ? 
-        <Styled.Circle value={this.props.size}>￥{this.props.price}</Styled.Circle>
-        :
-        <Styled.Ellipse value={this.props.size}>￥{this.props.price}</Styled.Ellipse>
+        this.props.type === 'circle' ?
+            <Styled.Circle value={this.props.size}>￥{this.props.price}</Styled.Circle>
+            :
+            <Styled.Ellipse value={this.props.size}>￥{this.props.price}</Styled.Ellipse>
     )
 
     public render() {
-        return(
-            <div>
+        return (
+            <div onClick={this.hundleClick}>
                 {this.renderPriceCard()}
             </div>
         )
